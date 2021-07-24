d0b0c6e64465e6b048f1ccb794bf4d63cf7c0f54
vykthr <victorawoke07@gmail.com>
2021-07-24 15:05:58 +0100

    updated repo

---
+++ data.js
@@ -0,0 +1,16 @@
+const films = [
+    // var videoLinks = ['ZdW-bsn9oYg', 'C2JIJOJfk0U', 'u5e6CnF_qv0', 'ygy620c-Tm0', 'dN8k7qZc7eY', 'iNqfjNZCsAI', 
+
+    { name: 'Empathetic Touch', link: 'ZdW-bsn9oYg' },
+    { name: 'Yo No Soy Este Cuerpo', link: 'C2JIJOJfk0U' },
+    { name: 'Water In My Lungs', link: 'u5e6CnF_qv0' },
+    { name: 'What If?', link: 'ygy620c-Tm0' },
+    { name: 'The Butterfly People', link: 'dN8k7qZc7eY' },
+    { name: 'Ritual in Film (dir. by @loreleiacuna)', link: 'iNqfjNZCsAI' },
+    { name: 'QUE ME DICEN LOS ARBOLES', link: 'g4Cy_hoDtlc' },
+]
+
+const musicVideos = [
+    { name: 'SwavvyTheReaper - No Hook PT.1 (Prod by: Tommy Zahh)', link: '44S-Kh4iLKI' },
+    { name: 'Anacarina - NUMB (Dir. by Lorelei Acuña)', link: 'BOJ2BHiAMD0'}
+]
